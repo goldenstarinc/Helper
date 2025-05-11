5db2f314eb7cc5c7e3a18f5772b1610cad5db75f
goldenstarinc <goldenstar.inc@yandex.ru>
2025-05-11 15:40:37 +0300

    got something

--- GoodDeeds_Interface/PostsPage.xaml.cs
+++ GoodDeeds_Interface/PostsPage.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CurrentSession = CurrentSessionDataProvider.CurrentSessionDataProvider;
+using PostsLoader = PostingService.PostsLoader;
+using PostFields = PostingService.PostFields;
 
 namespace GoodDeeds_Interface
 {
@@ -20,9 +23,36 @@ namespace GoodDeeds_Interface
     /// </summary>
     public partial class PostsPage : Page
     {
+        private PostsLoader _postsLoader;
         public PostsPage()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            CurrentSession.CurrentUserId = 17;
+            _postsLoader = new PostsLoader();
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadPosts();
+        }
+        private void LoadPosts()
+        {
+            List<PostFields> posts = _postsLoader.LoadPosts();
+            PostsListView.ItemsSource = posts;
+        }
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPosts();
+        }
+        private void RespondButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.Content = "Вы откликнулись!";
+                button.IsEnabled = false;
+                button.Foreground = new SolidColorBrush(Colors.Black);
+                button.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            }
         }
     }
 }
